dff4cda7f49f2e232fad039f55af61c037848ae5
ljw <15262283592@163.com>
2022-11-07 00:45:06 +0800

    fix: 1

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,21 +2,37 @@
  * @Author: ljw 15262283592@163.com
  * @Date: 2022-11-06 23:46:06
  * @LastEditors: ljw 15262283592@163.com
- * @LastEditTime: 2022-11-06 23:49:00
+ * @LastEditTime: 2022-11-07 00:42:52
  * @FilePath: \vue3-game\src\views\home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script setup>
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { useCounterStore } from '../stores/counter'
+const $router = useRouter();
+const counterStore = useCounterStore()
+
+const handleExit = () => {
+  document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  counterStore.token = ''
+  // 跳转到登录页
+  $router.push({
+    path: '/Login'
+  })
+}
 
 </script>
+
 <template>
   <div class="home">
     <h1>This is an about page</h1>
+    <button @click="handleExit">退出登录</button>
   </div>
 </template>
 
 <style>
-h1{
+.home {
   color: white;
 }
 </style>
